f50209f5287133d0480b80d0e24f1afc819728b4
Pedro Germano <pedrogermano232@gmail.com>
2024-10-16 08:12:11 -0300

    Ternario

---
+++ ternario/assets/js/main.js
@@ -0,0 +1,12 @@
+const userVip = 999;
+
+// if (userVip >= 999) {
+//   console.log('Vip');
+
+// } else {
+//   console.log('Normal')
+// }
+
+const nivelUser = userVip >= 1000 ? 'Vip' : 'normal'
+console.log(nivelUser);
+
